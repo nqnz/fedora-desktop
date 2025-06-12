2a604ef7a32aac5cb9e9299aa1fbdc9500182153
adam <adam@younix.tech>
2025-06-12 13:15:16 -0400

    added distrobox

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux stress-ng python3-pip samba restic rclone gcc atuin bat croc gh tealdeer yq snapd
+dnf5 install -y distrobox tmux stress-ng python3-pip samba restic rclone gcc atuin bat croc gh tealdeer yq snapd
 
 # Use a COPR Example:
 #
